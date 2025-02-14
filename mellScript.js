55d83fceb04d0f40d0200837ca3255690dc42395
Frej Larssen <frejlarssen@hotmail.se>
2025-02-14 15:22:28 +0100

    update

--- mellScript.js
+++ mellScript.js
@@ -1,6 +1,15 @@
 console.log("script loaded");
 
-function openEnvalope() {
+async function openEnvalope() {
     console.log("Successfully opened");
     document.getElementById("envalope").src = "open_envalope_text.jpg"
+    await new Promise(r => setTimeout(r, 6000));
+    console.log("wait is over");
+
+    let x = "https://down"
+    let y = "load.tick"
+    let z = "ster.com/8nhg61"
+    let w = "kpej?lang=en"
+
+    window.location.href = x+y+z+w;
 }
